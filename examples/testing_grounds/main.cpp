11417aad2537087f1fc01720413b8801bfe2dcde
Filip Vasiljevic <fvasiljevic@gmail.com>
2020-10-14 19:16:07 +0200

    Clean up testing_grounds code

--- examples/testing_grounds/main.cpp
+++ examples/testing_grounds/main.cpp
@@ -1,5 +1,7 @@
 #include <SFML/Window.hpp>
 
+void handle_events(sf::Window& window, sf::Event& event);
+
 int main()
 {
     sf::Window window(sf::VideoMode(800, 600), "Rinvid testing grounds");
@@ -7,16 +9,23 @@ int main()
 
     while (window.isOpen())
     {
-        if (window.pollEvent(event))
+        handle_events(window, event);
+    }
+
+    return 0;
+}
+
+void handle_events(sf::Window& window, sf::Event& event)
+{
+    while (window.pollEvent(event))
+    {
+        switch (event.type)
         {
-            switch (event.type)
-            {
-                case sf::Event::Closed:
-                    window.close();
-                    break;
-                default:
-                    break;
-            }
+            case sf::Event::Closed:
+                window.close();
+                break;
+            default:
+                break;
         }
     }
 }
