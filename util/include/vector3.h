c4a4d31ffc2a6ee91a5f3519b537385b4a7b9873
Filip Vasiljevic <fvasiljevic@gmail.com>
2020-10-15 17:43:30 +0200

    Add Vector3 struct

---
+++ util/include/vector3.h
@@ -0,0 +1,16 @@
+#ifndef UTIL_VECTOR3_H
+#define UTIL_VECTOR3_H
+
+namespace rinvid
+{
+
+struct Vector3
+{
+    float x;
+    float y;
+    float z;
+};
+
+} // namespace rinvid
+
+#endif // UTIL_VECTOR3_H
