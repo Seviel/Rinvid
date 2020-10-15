25a481cff07f468fb39744c44fe84d9228d6be8e
Filip Vasiljevic <fvasiljevic@gmail.com>
2020-10-15 19:09:08 +0200

    Add drawing of triangle to testing_grounds

--- examples/testing_grounds/main.cpp
+++ examples/testing_grounds/main.cpp
@@ -1,19 +1,33 @@
 #include <SFML/Window.hpp>
 
 #include "core/include/rinvid_gfx.h"
+#include "core/include/triangle_shape.h"
+#include "util/include/vector3.h"
 
 void handle_events(sf::Window& window, sf::Event& event);
 
 int main()
 {
     sf::Window window(sf::VideoMode(800, 600), "Rinvid testing grounds");
-    sf::Event  event;
-
     rinvid::RinvidGfx::set_viewport(0, 0, 800, 600);
+
+    sf::Event event;
+
+    window.setActive(true);
+
+    rinvid::TriangleShape triangle{rinvid::Vector3{-0.8F, -0.5F, 0.0F},
+                                   rinvid::Vector3{0.8F, -0.5F, 0.0F},
+                                   rinvid::Vector3{0.0F, 0.8F, 0.0F}};
 
     while (window.isOpen())
     {
         handle_events(window, event);
+
+        rinvid::RinvidGfx::clear_screen(0.2F, 0.4F, 0.4F, 1.0F);
+
+        triangle.draw();
+
+        window.display();
     }
 
     return 0;
@@ -27,6 +41,9 @@ void handle_events(sf::Window& window, sf::Event& event)
         {
             case sf::Event::Closed:
                 window.close();
+                break;
+            case sf::Event::Resized:
+                rinvid::RinvidGfx::set_viewport(0, 0, event.size.width, event.size.height);
                 break;
             default:
                 break;
