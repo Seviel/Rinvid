5d7a7a795b6e435426c879c3e8e0bea8caa53ac1
Filip Vasiljevic <fvasiljevic@gmail.com>
2020-11-30 22:21:18 +0100

    Refactor triangle and rectangle to inherit from FixedPolygonShape

--- core/include/triangle_shape.h
+++ core/include/triangle_shape.h
@@ -1,34 +1,19 @@
 #ifndef CORE_INCLUDE_TRIANGLE_SHAPE_H
 #define CORE_INCLUDE_TRIANGLE_SHAPE_H
 
+#include "core/include/fixed_polygon_shape.h"
 #include "core/include/shape.h"
 #include "util/include/vector2.h"
 
 namespace rinvid
 {
 
-struct _triangle_vertices_t
-{
-    float data[9];
-};
-
-class TriangleShape : public Shape
+class TriangleShape : public FixedPolygonShape<3>
 {
   public:
     TriangleShape(Vector2 vert1, Vector2 vert2, Vector2 vert3);
 
     virtual void draw() override;
-    virtual void move(const Vector2 move_vector) override;
-    virtual void set_position(const Vector2 vector) override;
-
-  private:
-    virtual void convert_to_opengl_coordinates() override;
-    virtual void calculate_origin() override;
-
-    Vector2              vert1_;
-    Vector2              vert2_;
-    Vector2              vert3_;
-    _triangle_vertices_t vertices;
 };
 
 } // namespace rinvid
