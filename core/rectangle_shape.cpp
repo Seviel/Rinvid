12d13ab871dba2eaf77f0b23c3e9b09cd84ccee8
Filip Vasiljevic <fvasiljevic@gmail.com>
2020-12-03 20:52:59 +0100

    Rename function that normalizes coordinates

--- core/rectangle_shape.cpp
+++ core/rectangle_shape.cpp
@@ -25,7 +25,7 @@ RectangleShape::RectangleShape(Vector2 vert1, Vector2 vert2, Vector2 vert3, Vect
 
 void RectangleShape::draw()
 {
-    convert_to_opengl_coordinates();
+    normalize_coordinates();
     std::int32_t color_location = glGetUniformLocation(RinvidGfx::get_default_shader(), "in_color");
     glUniform4f(color_location, color_.r, color_.g, color_.b, color_.a);
     glBindVertexArray(vao_);
