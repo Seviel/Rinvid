bb08b9acbb8dc41543d007b0373d0f792956a3e9
Filip Vasiljevic <fvasiljevic@gmail.com>
2020-12-03 21:03:06 +0100

    Add empty spaces for improved readability

--- core/rectangle_shape.cpp
+++ core/rectangle_shape.cpp
@@ -27,8 +27,10 @@ RectangleShape::RectangleShape(Vector2 top_left, Vector2 top_right, Vector2 bott
 void RectangleShape::draw()
 {
     normalize_coordinates();
+
     std::int32_t color_location = glGetUniformLocation(RinvidGfx::get_default_shader(), "in_color");
     glUniform4f(color_location, color_.r, color_.g, color_.b, color_.a);
+    
     glBindVertexArray(vao_);
     glDrawArrays(GL_QUADS, 0, 4);
     glBindVertexArray(0);
