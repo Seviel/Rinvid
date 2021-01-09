5e786a78eac5c9689a6146d949acc63bc8fb6f83
Filip Vasiljevic <fvasiljevic@gmail.com>
2021-01-09 20:04:52 +0100

    Add stb_image lib for loading images

---
+++ extern/stb_image.cpp
@@ -0,0 +1,11 @@
+/**********************************************************************
+ * Copyright (c) 2021, Filip Vasiljevic
+ * All rights reserved.
+ *
+ * This file is subject to the terms and conditions of the BSD 2-Clause
+ * License.  See the file LICENSE in the root directory of the Rinvid
+ * repository for more details.
+ **********************************************************************/
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "extern/include/stb_image.h"
