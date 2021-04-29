a75bb761d807741e81c50a1c5f8e52dee7f27f1a
Seviel <fvasiljevic@gmail.com>
2021-04-29 17:33:57 +0200

    Add buildifier for Windows

--- buildify.sh
+++ buildify.sh
@@ -2,4 +2,10 @@
 
 # Note: this script should be ran from repo root folder
 
-tools/buildifier/buildifier -r `find -iname BUILD`
+OS=`uname -s`
+
+if [ "$OS" = "Linux" ]; then
+    tools/buildifier/buildifier -r `find -iname BUILD`
+else
+    tools/buildifier/buildifier_win -r `find -iname BUILD`
+fi
