250e8a8aaf357071e618ef7423d36bd766153fe5
Seviel <fvasiljevic@gmail.com>
2021-05-15 12:03:46 +0200

    Add comment to clang-format script

--- apply_clang_format.sh
+++ apply_clang_format.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+
+# Note: this script should be ran from repo root folder
+
 clang-format-6.0 -i `find . -type f \( -iname "*.cpp" ! -path "./extern/*" \)`
 clang-format-6.0 -i `find . -type f \( -iname "*.h" ! -path "./extern/*" \)`
