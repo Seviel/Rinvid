507b86d4dc992fa551a8e4aa84b25ba1902463c4
Filip Vasiljevic <fvasiljevic@gmail.com>
2022-08-14 19:35:17 +0200

    Add screens example

---
+++ examples/screens/level_two.cpp
@@ -0,0 +1,62 @@
+/**********************************************************************
+ * Copyright (c) 2022, Filip Vasiljevic
+ * All rights reserved.
+ *
+ * This file is subject to the terms and conditions of the BSD 2-Clause
+ * License.  See the file LICENSE in the root directory of the Rinvid
+ * repository for more details.
+ **********************************************************************/
+
+#include <SFML/Window.hpp>
+
+#include "level_two.h"
+#include "util/include/vector2.h"
+
+LevelTwo::LevelTwo(rinvid::Sprite* guardian_sprite) : guardian_sprite_{guardian_sprite}
+{
+}
+
+void LevelTwo::create()
+{
+    guardian_sprite_->set_position(rinvid::Vector2<float>{250.0F, 425.0F});
+    guardian_sprite_->play("standing_right");
+}
+
+void LevelTwo::update(double delta_time)
+{
+    rinvid::RinvidGfx::clear_screen(0.0F, 0.0F, 0.0F, 1.0F);
+
+    bg_level_two_sprite.draw();
+    guardian_sprite_->draw(delta_time);
+
+    static bool last_moved_right = true;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) ||
+        sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+    {
+        guardian_sprite_->move({3.0F, 0.0F});
+        guardian_sprite_->play("treading_right");
+        last_moved_right = true;
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) ||
+             sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+    {
+        guardian_sprite_->move({-3.0F, 0.0F});
+        guardian_sprite_->play("treading_left");
+        last_moved_right = false;
+    }
+    else
+    {
+        if (last_moved_right)
+        {
+            guardian_sprite_->play("standing_right");
+        }
+        else
+        {
+            guardian_sprite_->play("standing_left");
+        }
+    }
+}
+
+void LevelTwo::destroy()
+{
+}
