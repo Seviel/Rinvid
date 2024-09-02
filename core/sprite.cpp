263bf316268d902b8a0b2ef97bdd6141c7b89493
Filip Vasiljevic <fvasiljevic@gmail.com>
2024-09-02 22:48:03 +0200

    Tests: Add some basic sprite unit tests

--- core/sprite.cpp
+++ core/sprite.cpp
@@ -13,8 +13,8 @@ namespace rinvid
 {
 
 Sprite::Sprite()
-    : sprite_animation_{}, texture_{nullptr}, width_{}, height_{}, top_left_{}, texture_offset_{},
-      opacity_{1.0F}
+    : sprite_animation_{}, texture_{nullptr}, width_{0}, height_{0}, top_left_{0.0F, 0.0F},
+      texture_offset_{0.0F, 0.0F}, opacity_{1.0F}
 {
 }
 
