3a4c4e06191fb2a189c53a9a444d3a51de1c8ed7
Filip Vasiljevic <fvasiljevic@gmail.com>
2024-09-07 17:21:38 +0200

    Update clang-format version and apply the script

--- apply_clang_format.sh
+++ apply_clang_format.sh
@@ -2,5 +2,5 @@
 
 # Note: this script should be ran from repo root folder
 
-clang-format-6.0 -i `find . -type f \( -iname "*.cpp" ! -path "./extern/*" \)`
-clang-format-6.0 -i `find . -type f \( -iname "*.h" ! -path "./extern/*" \)`
+clang-format-12 -i `find . -type f \( -iname "*.cpp" ! -path "./extern/*" \)`
+clang-format-12 -i `find . -type f \( -iname "*.h" ! -path "./extern/*" \)`
