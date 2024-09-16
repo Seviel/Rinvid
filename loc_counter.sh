9ee15524b1a22dd44a3ed2d1ffe51ab5673b5757
Filip Vasiljevic <fvasiljevic@gmail.com>
2024-09-16 21:28:49 +0200

    Scripts: Update loc_counter.sh
    Count in .sh and .py lines

--- loc_counter.sh
+++ loc_counter.sh
@@ -4,13 +4,21 @@
 # Run from root repo directory
 
 cpp_loc=`find -iname "*.cpp" -not -path "./extern/*" | xargs cat | wc -l`
+cxx_loc=`find -iname "*.cxx" -not -path "./extern/*" | xargs cat | wc -l`
 h_loc=`find -iname "*.h" -not -path "./extern/*" | xargs cat | wc -l`
+hpp_loc=`find -iname "*.hpp" -not -path "./extern/*" | xargs cat | wc -l`
 vert_loc=`find -iname "*.vert" -not -path "./extern/*" | xargs cat | wc -l`
 frag_loc=`find -iname "*.frag" -not -path "./extern/*" | xargs cat | wc -l`
+sh_loc=`find -iname "*.sh" -not -path "./extern/*" | xargs cat | wc -l`
+py_loc=`find -iname "*.py" -not -path "./extern/*" | xargs cat | wc -l`
 
 echo ".cpp: " $cpp_loc
+echo ".cxx: " $cxx_loc
 echo ".h: " $h_loc
+echo ".hpp: " $hpp_loc
 echo ".vert: " $vert_loc
 echo ".frag: " $frag_loc
+echo ".sh: " $sh_loc
+echo ".py: " $py_loc
 
-echo "Total: "$(($cpp_loc + $h_loc + $vert_loc + $frag_loc))
+echo "Total: "$(($cpp_loc + $cxx_loc + $h_loc + $hpp_loc + $vert_loc + $frag_loc + $sh_loc + $py_loc))
