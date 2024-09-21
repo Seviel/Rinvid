78a6284b6c7be7d9a6eab9a542b9113b2e1a4858
Filip Vasiljevic <fvasiljevic@gmail.com>
2024-09-21 17:41:32 +0200

    CI: Add verification of code formatting to CI

---
+++ verify_format.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+
+def is_clang_formatted(file_path):
+    result = subprocess.run(['clang-format-11', '-output-replacements-xml', file_path], capture_output=True, text=True)
+    return '<replacement ' not in result.stdout
+
+def check_repo_for_clang_format(repo_path):
+    for root, dirs, files in os.walk(repo_path):
+        if 'extern' in root:
+            continue
+        for file in files:
+            if file.endswith(('.cpp', '.h')):            
+                file_path = os.path.join(root, file)                
+                if not is_clang_formatted(file_path):
+                    print(f"File {file_path} is not clang-formatted.")
+                    return False
+    print("All files are clang-formatted.")
+    return True
+    
+if __name__ == "__main__":
+    repo_path = './'
+    
+    if check_repo_for_clang_format(repo_path) == True:
+        exit(0)
+    
+    exit(1)
